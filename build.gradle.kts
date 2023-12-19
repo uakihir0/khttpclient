840a638948ed81b47452dc1e2919ebd4332be6da
Akihiro Urushiara <a.urusihara@gmail.com>
2023-12-20 02:39:55 +0900

    change maven url.

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,7 @@ tasks.named<Test>("jvmTest") {
 publishing {
     repositories {
         maven {
-            url = uri("https://repo.repsy.io/mvn/uakihir0/khttpclient")
+            url = uri("https://repo.repsy.io/mvn/uakihir0/public")
             credentials {
                 username = System.getenv("USERNAME")
                 password = System.getenv("PASSWORD")
