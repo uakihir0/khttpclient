f41a09d2e92d790a62d1d38ad7d17adb4df48dbe
URUSHIHARA Akihiro <a.urusihara@gmail.com>
2024-10-03 03:48:56 +0900

    bump up version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 }
 
 group = "work.socialhub"
-version = "0.0.2-SNAPSHOT"
+version = "0.0.3-SNAPSHOT"
 
 gitVersioning.apply {
     refs {
@@ -146,17 +146,20 @@ mavenPublishing {
             javadocJar = JavadocJar.Dokka("dokkaHtml")
         )
     )
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    publishToMavenCentral(
+        host = SonatypeHost.CENTRAL_PORTAL,
+        automaticRelease = true,
+    )
+
     if (project.hasProperty("mavenCentralUsername") ||
-        System.getenv("ORG_GRADLE_PROJECT_mavenCentralUsername") != null) {
-        signAllPublications()
-    }
+        System.getenv("ORG_GRADLE_PROJECT_mavenCentralUsername") != null
+    ) signAllPublications()
 }
 
 signing {
     if (project.hasProperty("mavenCentralUsername") ||
-        System.getenv("ORG_GRADLE_PROJECT_mavenCentralUsername") != null) {
-        useGpgCmd()
-    }
+        System.getenv("ORG_GRADLE_PROJECT_mavenCentralUsername") != null
+    ) useGpgCmd()
 }
 
