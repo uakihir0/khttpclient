aea74325bd0fea94521d2919800782fdb38936d8
Akihiro Urushihara <a.urusihara@gmail.com>
2025-11-11 11:23:03 +0900

    Adjust maven publishing for new plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 import com.vanniktech.maven.publish.JavadocJar
 import com.vanniktech.maven.publish.KotlinMultiplatform
-import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.konan.target.HostManager
 
 plugins {
@@ -162,7 +161,6 @@ mavenPublishing {
     )
 
     publishToMavenCentral(
-        host = SonatypeHost.CENTRAL_PORTAL,
         automaticRelease = true,
     )
 
